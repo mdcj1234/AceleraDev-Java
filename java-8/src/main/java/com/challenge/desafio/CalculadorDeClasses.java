8efd242356fcfabc6699e0f004835e1645d6861f
Marcio <mdcj1234@gmail.com>
2020-05-03 13:59:10 -0300

    Desafio semana 100%

---
+++ java-8/src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -0,0 +1,58 @@
+package com.challenge.desafio;
+import com.challenge.annotation.Somar;
+import com.challenge.annotation.Subtrair;
+import com.challenge.interfaces.Calculavel;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+
+public class CalculadorDeClasses implements Calculavel {
+
+    @Override
+    public BigDecimal somar(Object o) {
+
+        BigDecimal soma = BigDecimal.ZERO;
+
+        for(Field field : o.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            if(field.isAnnotationPresent(Somar.class) && field.getType().equals(BigDecimal.class)){
+
+                try {
+                    soma = soma.add((BigDecimal) field.get(o));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return soma;
+    }
+
+    @Override
+    public BigDecimal subtrair(Object o) {
+
+        BigDecimal soma = BigDecimal.ZERO;
+
+        for(Field field : o.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            if(field.isAnnotationPresent(Subtrair.class) && field.getType().equals(BigDecimal.class)){
+
+                try {
+                    soma = soma.add((BigDecimal) field.get(o));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return soma;
+    }
+
+    @Override
+    public BigDecimal totalizar(Object o) {
+
+        BigDecimal soma = somar(o);
+        BigDecimal sub = subtrair(o);
+
+        return soma.subtract(sub);
+    }
+}
